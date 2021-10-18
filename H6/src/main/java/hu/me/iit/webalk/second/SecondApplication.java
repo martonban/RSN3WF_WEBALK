3743ab6ef222decaaff14cfb65bf2bd5a502c283
Bán Márton <marton.bn@gmail.com>
2021-10-19 00:37:59 +0200

    H6 project(Nem tudom miért nem commitelte már ezeket)

---
+++ H6/src/main/java/hu/me/iit/webalk/second/SecondApplication.java
@@ -0,0 +1,13 @@
+package hu.me.iit.webalk.second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondApplication.class, args);
+	}
+
+}
