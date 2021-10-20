87cc3092ff7aaacd621ba4c76577884075bb0165
Bán Márton <marton.bn@gmail.com>
2021-10-20 14:36:22 +0200

    WIP

---
+++ H7/src/main/java/hu/me/iit/webalk/Third/Dependency.java
@@ -0,0 +1,9 @@
+package hu.me.iit.webalk.Third;
+
+public interface Dependency {
+    void helpNoParameterNoReturnValue();
+    int helpNoParameterReturnValue();
+    int helpParameterReturnValue(int a);
+    void helpParameterNoReturnValue(int a);
+    String important(String a);
+}
---
+++ H7/src/main/java/hu/me/iit/webalk/Third/DependencyImp.java
@@ -0,0 +1,25 @@
+package hu.me.iit.webalk.Third;
+
+public class DependencyImp implements Dependency{
+
+    @Override
+    public void helpNoParameterNoReturnValue() {}
+
+    @Override
+    public int helpNoParameterReturnValue() {
+        return 0;
+    }
+
+    @Override
+    public int helpParameterReturnValue(int a) {
+        return 0;
+    }
+
+    @Override
+    public void helpParameterNoReturnValue(int a) {}
+
+    @Override
+    public String important(String a) {
+        return "<b> " + a + " </b>";
+    }
+}
